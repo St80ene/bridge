c2a92c98504ca0e5f1cc75910fde6ffd81e92ce9
etiene <etienejames5@gmail.com>
2023-11-12 06:11:29 +0100

    FORMATTING
    Signed-off-by: etiene <etienejames5@gmail.com>

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -15,7 +15,8 @@ const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'logs/error.log' }),
+    new winston.transports.File({ filename: 'logs/info.log', level: 'info' }),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
   ],
 });
 
